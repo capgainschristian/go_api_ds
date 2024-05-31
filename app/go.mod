9209fdbf3c203bdefd8ed8bd5711097679942854
Christian Graham <capgainschristian@gmail.com>
2024-05-31 12:54:00 -0400

    added same example for all CRUD functionalities

--- app/go.mod
+++ app/go.mod
@@ -1,22 +1,26 @@
 module github.com/capgainschristian/go_api_ds
 
-go 1.21
-
-toolchain go1.22.3
+go 1.19
 
 require (
 	github.com/brianvoe/gofakeit/v6 v6.28.0
 	github.com/gorilla/mux v1.8.1
+	github.com/stretchr/testify v1.8.1
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.10
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- app/handlers/handlers_test.go
+++ app/handlers/handlers_test.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/capgainschristian/go_api_ds/database"
+	"github.com/capgainschristian/go_api_ds/models"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
-	database.ConnectDb
+	database.ConnectDb()
 
 	code := m.Run()
 
@@ -14,7 +23,35 @@ func TestMain(m *testing.M) {
 }
 
 func TestAddCustomer(t *testing.T) {
-	router := mux.NewRouter()
-	
+	//database.DB.Db.Exec("DELETE FROM customers")
 
+	router := mux.NewRouter()
+	router.HandleFunc("/customer", AddCustomer).Methods("POST")
+
+	customer := &models.Customer{
+		Name:    "Christian Graham",
+		Email:   "christian.graham@grahamsummitllc.com",
+		Address: "777 Summit LLC Drive",
+		Number:  1111,
+	}
+
+	jsonCustomer, _ := json.Marshal(customer)
+
+	req, err := http.NewRequest("POST", "/customer", bytes.NewBuffer(jsonCustomer))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusAccepted, rr.Code)
+	expected := "Customer added successfully."
+	assert.Equal(t, expected, rr.Body.String())
+
+	// Verify customer was added
+	var count int64
+	database.DB.Db.Model(models.Customer{}).Where("email = ?", customer.Email).Count(&count)
+	assert.Equal(t, int64(1), count)
 }
