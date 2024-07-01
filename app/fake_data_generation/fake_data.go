d7dfd10191bbac900c6ae7e572f29f529808fce2
Christian Graham <capgainschristian@gmail.com>
2024-07-01 15:52:35 -0400

    refactored fake data creation to run concurrently using go channels

--- app/fake_data_generation/fake_data.go
+++ app/fake_data_generation/fake_data.go
@@ -5,17 +5,92 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"sync"
 
 	"github.com/brianvoe/gofakeit/v6"
-	"github.com/capgainschristian/go_api_ds/models"
 )
 
+type Customer struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+	Number  int    `json:"number"`
+}
+
 func main() {
-	// Seed the random generator for consistent results (optional)
 	gofakeit.Seed(0)
 
 	numDataPoints := 100
+	dataPoints := generateDataPoints(numDataPoints)
 
+	// Create a channel to communicate results
+	resultChan := make(chan error, numDataPoints)
+
+	// Use a wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+	wg.Add(numDataPoints)
+
+	// Launch goroutines to handle customer creation concurrently
+	for _, dataPoint := range dataPoints {
+		go func(dp map[string]interface{}) {
+			defer wg.Done()
+
+			address := fmt.Sprintf("%s, %s, %s, %s",
+				dp["street"],
+				dp["city"],
+				dp["state"],
+				dp["zip"],
+			)
+
+			customer := Customer{
+				Name:    dp["name"].(string),
+				Email:   dp["email"].(string),
+				Address: address,
+				Number:  dp["number"].(int),
+			}
+
+			jsonData, err := json.Marshal(customer)
+			if err != nil {
+				log.Printf("Error marshaling JSON: %v\nData Point: %v\n", err, dp)
+				resultChan <- err
+				return
+			}
+
+			curlCommand := exec.Command("curl",
+				"--header", "Content-Type: application/json",
+				"--request", "POST",
+				"--data", string(jsonData),
+				"http://localhost:3000/customercreation",
+			)
+
+			output, err := curlCommand.CombinedOutput()
+			if err != nil {
+				log.Printf("Error executing curl command: %v\nOutput: %s\nData Point: %v\n", err, output, dp)
+				resultChan <- err
+				return
+			}
+
+			log.Printf("Customer added successfully: %s\n", customer.Name)
+			resultChan <- nil // Successfully added customer
+		}(dataPoint)
+	}
+
+	// Close the result channel after all goroutines finish
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	// Process results from result channel
+	for result := range resultChan {
+		if result != nil {
+			log.Printf("Error adding customer: %v\n", result)
+		}
+	}
+}
+
+// Function to generate random data points
+func generateDataPoints(numDataPoints int) []map[string]interface{} {
 	dataPoints := make([]map[string]interface{}, numDataPoints)
 
 	for i := 0; i < numDataPoints; i++ {
@@ -31,44 +106,7 @@ func main() {
 		dataPoint["number"] = gofakeit.Number(1, 100)
 
 		dataPoints[i] = dataPoint
-
-		// Need to capture address as string for CURL
-		address := fmt.Sprintf("%s, %s, %s, %s",
-			dataPoint["street"],
-			dataPoint["city"],
-			dataPoint["state"],
-			dataPoint["zip"],
-		)
-
-		// Prepare customer data using the random dataPoints
-		customer := models.Customer{
-			Name:    dataPoint["name"].(string),
-			Email:   dataPoint["email"].(string),
-			Address: address,
-			Number:  dataPoint["number"].(int),
-		}
-
-		// Send as JSON
-		jsonData, err := json.Marshal(customer)
-		if err != nil {
-			log.Printf("Error marshaling JSON: %v\nData Point: %v\n", err, dataPoint)
-		}
-
-		curlCommand := exec.Command("curl",
-			"--header", "Content-Type: application/json",
-			"--request", "POST",
-			"--data", string(jsonData),
-			"http://localhost:3000/customercreation",
-		)
-
-		output, err := curlCommand.CombinedOutput()
-
-		if err != nil {
-			log.Printf("Error executing curl command: %v\nOutput: %s\nData Point: %v\n", err, output, dataPoint)
-			continue
-		}
-
-		log.Printf("Customer added successfully: %s\n", customer.Name)
-
 	}
+
+	return dataPoints
 }
